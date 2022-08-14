f11b0d41ded9dca04f7a72ec5ffd86b0ee9581af
Tom Mills <t.mills124@gmail.com>
2022-08-14 15:19:47 +0100

    created testing and solution for artist table on set up + ability to insert into artist table using post method

--- src/controllers/artist.js
+++ src/controllers/artist.js
@@ -1,3 +1,17 @@
-exports.create = (req, res) => {
+const getDb = require('../services/db');
+
+exports.create = async (req, res) => {
+    const db = await getDb();
+    const { name, genre } = req.body
+
+    try {
+
+    await db.query(
+        `INSERT INTO Artist (name, genre) VALUES ('${name}', '${genre}')`);
     res.sendStatus(201);
+    } catch (err) {
+    res.sendStatus(500).json(err);
+    }
+
+    db.end();
   };
